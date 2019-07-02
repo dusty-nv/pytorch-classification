3c1ae91cd2dbc680cf3b8b114acac46414433530
dusty-nv <dustinf@nvidia.com>
2019-07-02 15:39:50 -0400

    updated ONNX validate

--- onnx_validate.py
+++ onnx_validate.py
@@ -9,7 +9,7 @@ import argparse
 import sys
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--model', type=str, default='resnet-18.onnx')
+parser.add_argument('--model', type=str, default='resnet18.onnx')
 args = parser.parse_args(sys.argv[1:])
 
 # Load the ONNX model
