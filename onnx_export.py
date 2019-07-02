35254265c28b045d5e7ac61eddf5b5690493b970
dusty-nv <dustinf@nvidia.com>
2019-07-02 15:39:39 -0400

    updated ONNX exporter

--- onnx_export.py
+++ onnx_export.py
@@ -13,9 +13,11 @@ model_names = sorted(name for name in models.__dict__
 
 # exporter settings
 parser = argparse.ArgumentParser()
-parser.add_argument('--model_in', type=str, default='model_best.pth.tar')
-parser.add_argument('--model_out', type=str, default='resnet-18.onnx')
-parser.add_argument('--image', type=str, required=True, help='input image to use')
+parser.add_argument('--model_in', type=str, default='model_best.pth.tar', help="name of input PyTorch model (default: model_best.pth.tar)")
+parser.add_argument('--model_out', type=str, default='', help="name of output ONNX model (default: <ARCH>.onnx)")
+parser.add_argument('--input_width', type=int, default=224, help="resolution of input image expected by the model (default: 224)")
+parser.add_argument('--input_height', type=int, default=224, help="resolution of input image expected by the model (default: 224)")
+#parser.add_argument('--image', type=str, required=True, help='example input image to use')
 parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet18',
                     choices=model_names,
                     help='model architecture: ' +
@@ -25,18 +27,21 @@ parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet18',
 opt = parser.parse_args() 
 print(opt)
 
+if not opt.model_out:
+	opt.model_out = opt.arch + '.onnx'
 
 # set the device
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print('running on device ' + str(device))
 
 
-# load the image
-img = Image.open(opt.image)
-img_to_tensor = ToTensor()
-input = img_to_tensor(img).view(1, -1, img.size[1], img.size[0]).to(device)
+# create example image data
+input = torch.ones((1, 3, opt.input_height, opt.input_width)).cuda()
+#img = Image.open(opt.image)
+#img_to_tensor = ToTensor()
+#input = img_to_tensor(img).view(1, -1, img.size[1], img.size[0]).to(device)
 
-print('input image size {:d}x{:d}'.format(img.size[0], img.size[1]))
+print('input image size {:d}x{:d}'.format(opt.input_width, opt.input_height))
 
 
 # load the model
